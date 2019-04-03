982d9faca0ca602e25e6a15d78a01cf1fad55598
Sindri Már Kaldal Sigurjónsson <sindrimars@gmail.com>
2019-04-03 11:38:11 +0000

    added scroll functionality to nav buttons. Funky color when clicked

--- src/components/navigation-bar/NavigationBar.js
+++ src/components/navigation-bar/NavigationBar.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
 import './NavigationBar.css';
+import ScrollIntoView from 'react-scroll-into-view'
+
 
 class NavigationBar extends Component {
   render() {
     return (
         <div className="navigationBar">
             <ul>
-              <li>Home</li>
-              <li>CV</li>
-              <li>Projects</li>
+              <li>
+                <ScrollIntoView selector="#home">
+                  <button>Home</button>
+                </ScrollIntoView>
+              </li>
+              <li>
+                <ScrollIntoView selector="#cv">
+                  <button>CV</button>
+                </ScrollIntoView>
+              </li>
+              <li>
+                <ScrollIntoView selector="#projects">
+                  <button>Projects</button>
+                </ScrollIntoView>
+              </li>
             </ul>
         </div>  
     );
   }
-}
+} 
 
 export default NavigationBar;
